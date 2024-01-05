6bb0669a6494dec49e35dbb84790aa18664a0791
SokolovNetology <sokolov180375@mail.ru>
2024-01-05 18:24:34 +0300

    Geo-Service

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -1,5 +1,22 @@
 package ru.netology.sender;
 
-class MessageSenderImplTest {
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.i18n.LocalizationServiceImpl;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageSenderImplTest {
+    @Mock
+     GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
+     LocalizationServiceImpl localizationService = Mockito.mock(LocalizationServiceImpl.class);
+
+    @Test
+    void send() {
+        Mockito.doReturn("96").when(localizationService).locale(Country.USA);
+        Mockito.doReturn("172").when(localizationService).locale(Country.RUSSIA);
+    }
 }
